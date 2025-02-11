988f8219ef513cef1b0a6684ab0db6b24359e475
dplochcoder <daniel.ploch.coder@gmail.com>
2025-02-11 15:40:04 -0500

    Implement checkpoints

--- HK8YPlando/IC/BrettasHouse.cs
+++ HK8YPlando/IC/BrettasHouse.cs
@@ -1,5 +1,7 @@
-﻿using ItemChanger;
+﻿using HK8YPlando.Scripts.Framework;
+using ItemChanger;
 using ItemChanger.Extensions;
+using ItemChanger.FsmStateActions;
 using ItemChanger.Modules;
 using System.Collections.Generic;
 using System.Text;
@@ -16,9 +18,14 @@ internal record HeartDoorData
 
 internal class BrettasHouse : Module
 {
+    private static readonly FsmID dreamNailId = new("Dream Nail");
+
     public int Hearts = 0;
     public Dictionary<string, HeartDoorData> DoorData = [];
-    public bool ReachedHouse = false;
+
+    public string CheckpointScene = "BrettaHouseEntry";
+    public string CheckpointGate = "right1";
+    public int CheckpointPriority = 0;
 
     public static BrettasHouse Get() => ItemChangerMod.Modules.Get<BrettasHouse>()!;
 
@@ -40,12 +47,14 @@ internal class BrettasHouse : Module
     {
         if (GetTracker(out var t)) t.OnGenerateFocusDesc += ShowHeartsInInventory;
         Events.AddSceneChangeEdit("Town", RedirectBrettaDoor);
+        Events.AddFsmEdit(dreamNailId, EditDreamNail);
     }
 
     public override void Unload()
     {
         if (GetTracker(out var t)) t.OnGenerateFocusDesc -= ShowHeartsInInventory;
         Events.RemoveSceneChangeEdit("Town", RedirectBrettaDoor);
+        Events.RemoveFsmEdit(dreamNailId, EditDreamNail);
     }
 
     private void ShowHeartsInInventory(StringBuilder sb)
@@ -59,11 +68,30 @@ internal class BrettasHouse : Module
 
     private void RedirectBrettaDoor(Scene scene)
     {
-        if (ReachedHouse) return;
-
         var obj = scene.FindGameObject("bretta_house")!.FindChild("open")!.FindChild("door_bretta")!;
         var vars = obj.LocateMyFSM("Door Control").FsmVariables;
-        vars.FindFsmString("New Scene").Value = "BrettaHouseEntry";
-        vars.FindFsmString("Entry Gate").Value = "right1";
+        vars.FindFsmString("New Scene").Value = CheckpointScene;
+        vars.FindFsmString("Entry Gate").Value = CheckpointGate;
     }
+
+    private HashSet<BrettaCheckpoint> activeCheckpoints = [];
+
+    internal void LoadCheckpoint(BrettaCheckpoint checkpoint)
+    {
+        activeCheckpoints.Add(checkpoint);
+
+        if (checkpoint.Priority > CheckpointPriority)
+        {
+            CheckpointPriority = checkpoint.Priority;
+            CheckpointGate = checkpoint.EntryGate!;
+            CheckpointScene = checkpoint.gameObject.scene.name;
+        }
+    }
+
+    internal void UnloadCheckpoint(BrettaCheckpoint checkpoint) => activeCheckpoints.Remove(checkpoint);
+
+    internal void EditDreamNail(PlayMakerFSM fsm) => fsm.GetState("Can Set?")?.AddFirstAction(new Lambda(() =>
+        {
+            if (activeCheckpoints.Count > 0) fsm.SendEvent("FAIL");
+        }));
 }
---
+++ UnityScriptShims/Scripts/Generated/Framework/BrettaCheckpoint.cs
@@ -0,0 +1,9 @@
+namespace HK8YPlando.Scripts.Framework
+{
+    public class BrettaCheckpoint : UnityEngine.MonoBehaviour
+    {
+        public int Priority;
+        public string EntryGate;
+        
+    }
+}
