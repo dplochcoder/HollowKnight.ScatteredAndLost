bc0e1803dd1954d686ce79c52476acc5c34ea996
dplochcoder <daniel.ploch.coder@gmail.com>
2025-04-05 21:14:48 -0400

    Tidy rando menu

--- ScatteredAndLost/ScatteredAndLostSettings.cs
+++ ScatteredAndLost/ScatteredAndLostSettings.cs
@@ -17,8 +17,8 @@ public class RandomizerSettings
     public bool Enabled = false;
     public bool EnableCheckpoints = true;
     public bool RandomizeSoulTotems = false;
-    public bool EnableHeartDoors = true;
     public bool EnablePreviews = true;
+    public bool EnableHeartDoors = true;
 
     [DynamicBound(nameof(MaxHearts), true)]
     [MenuRange(1, 50)]
