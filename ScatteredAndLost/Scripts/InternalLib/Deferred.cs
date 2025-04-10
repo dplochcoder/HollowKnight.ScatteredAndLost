8860a96854a6cc0a7e732cc34042319b9bc526ae
dplochcoder <daniel.ploch.coder@gmail.com>
2025-04-10 11:41:48 -0400

    Port Deferred class from SotV

---
+++ ScatteredAndLost/Scripts/InternalLib/Deferred.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HK8YPlando.Scripts.InternalLib;
+
+// A deferred object which may not exist yet.
+//
+// Actions can be delegated to it immediately or after creation.
+public class Deferred<T>
+{
+    private List<Action<T>> actions = [];
+    private T? target;
+
+    public Deferred() { }
+    public Deferred(T target) => this.target = target;
+
+    public void Set(T target)
+    {
+        this.target = target;
+
+        actions.ForEach(a => a(target));
+        actions.Clear();
+    }
+
+    public void Link(Deferred<T> src) => src.Do(Set);
+
+    public void Do(Action<T> action)
+    {
+        if (target == null) actions.Add(action);
+        else action(target);
+    }
+
+    public Deferred<U> Map<U>(Func<T, U> transformer)
+    {
+        Deferred<U> lazy = new();
+        Do(t => lazy.Set(transformer(t)));
+        return lazy;
+    }
+}
